1c512a8aa76c7e735db4c58ed8574ab6126bc07c
Pedro Gabriel <gabriellimadossantosp@gmail.com>
2026-06-02 18:45:24 +0000

    terminei o exercício do boleto de gigi

--- aula_12/ex2.py
+++ aula_12/ex2.py
@@ -58,3 +58,97 @@ class boleto:
         s += f"Data de Pagamento: {self.__data_pagamento}"
         s += f"Situação: {self.__situacao_pagamento}"
         return s
+
+class boletoUI:
+    __boletos = []
+
+    @staticmethod
+    def main():
+        op = 0
+        while op != 0:
+            op = boletoUI.menu()
+            if op == 1: boletoUI.inserir()
+            if op == 2: boletoUI.listar()
+            if op == 3: boletoUI.atualizar()
+            if op == 4: boletoUI.excluir()
+            if op == 5: boletoUI.boletos_em_aberto()
+            if op == 6: boletoUI.boletos_pagos()
+            if op == 7: boletoUI.boletos_a_vencer()
+            if op == 8: boletoUI.boletos_vencidos()
+            if op == 9: boletoUI.pagar_boletos()
+
+    @staticmethod
+    def menu():
+        print("---------------------------------------------------")
+        print("1 - inserir, 2 - Listar, 3 - Atualizar, 4 - Excluir")
+        print("5 - Boletos em aberto, 6 - Boletos pagos")
+        print("7 - Boletos a vencer, 8 - Boletos vencidos")
+        print("9 - Pagar boletos, 10 - Fim")
+        print("---------------------------------------------------")
+        return int(input("Escolha uma opção: "))
+    
+    @classmethod
+    def inserir(cls):
+        codigo = input("Informe o código do boleto com 10 dígitos: ")
+        emissao = datetime.strptime(input("Informe a data de emissão dd/mm/aaaa"), "%d/%m/%Y")
+        vencimento = datetime.strptime(input("Informe a data de vencimento dd/mm/aaaa"), "%d/%m/%Y")
+        valor = float(input("Informe o valor: "))
+        x = boleto(codigo, emissao, vencimento, valor)
+        cls.__boletos.append(x)
+    
+    @classmethod
+    def listar(cls):
+        for x in cls.__boletos: print(x)
+
+    @classmethod
+    def atualizar(cls):
+        codigo = input("Informe o código: ")
+        for x in cls.__boletos:
+            if x.get_cod_barras == codigo:
+                x.set_data_emissao(datetime.strptime(input("Informe a nova data de emissão: "), "%d/%m/%Y"))
+                x.set_data_vencimento(datetime.strptime(input("Informe a nova data de vencimento: "), "%d/%m/%Y"))
+                x.set_valor_boleto(float(input("Informe o novo valor: ")))
+
+    @classmethod
+    def excluir(cls):
+        codigo = int(input("Informe o código de barras do boleto: "))
+        for x in cls.__boletos:
+            if x.get_cod_barras() == codigo:
+                cls.__boletos.remove(x)
+
+    @classmethod
+    def boletos_em_aberto(cls):
+        for x in cls.__boletos:
+            if x.get_situacao_pagamento() == pagamento.EM_ABERTO and x.get_data_vencimento() > datetime.now():
+                print(x)
+
+    @classmethod
+    def boletos_pagos(cls):
+        for x in cls.__boletos:
+            if x.get_situacao_pagamento() == pagamento.PAGO:
+                print(x)
+
+    @classmethod
+    def boletos_a_vencer(cls):
+        for x in cls.__boletos:
+            if x.get_situacao_pagamento() == pagamento.EM_ABERTO and x.get_data_vencimento() > datetime.now():
+                print(x)
+
+    @classmethod
+    def boletos_vencidos(cls):
+        for x in cls.__boletos:
+            if x.get_situacao_pagamento() == pagamento.EM_ABERTO and x.get_data_vencimento() < datetime.now():
+                print(x)
+
+    @classmethod
+    def pagar_boletos(cls):
+        for x in cls.__boletos:
+            codigo = int(input("Informe o código: "))
+            for x in cls.__boletos:
+                if x.get_cod_barras() == codigo:
+                    if x.get_situacao_pagamento() == pagamento.EM_ABERTO:
+                        valor = float(input(f"Valor do boleto é R${x.get_valor_boleto():.2f}. Digite o valor pago: "))
+                        x.pagar(valor)
+                        print("Pagamento efetuado")
+
+boletoUI.main()
