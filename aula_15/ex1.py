b4d498ae002d6642e92131c9b46a97cd0cbea81f
Pedro Gabriel <gabriellimadossantosp@gmail.com>
2026-06-18 17:28:06 +0000

    Esse código ainda tá bem incompleto, depois eu deixo tudo certinho

---
+++ aula_15/ex1.py
@@ -0,0 +1,11 @@
+x = {"RN" : "Natal", "PB" : "João Pessoa", "PE" : "Recife"}
+x["AM"] = "Manaus"
+# x[5] = "teste"
+y = {"PB"} = "J. Pessoa"
+print(x)
+print(*x)
+print(len(x))
+print(max(x))
+
+for item in x.items(): print(item)
+for item in x: print(item)
