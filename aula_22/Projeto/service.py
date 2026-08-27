bdc12335025b5deeb1e12747fb366261ad5db677
Pedro Gabriel <gabriellimadossantosp@gmail.com>
2026-08-27 17:21:21 +0000

    ainda tenho que terminar isso aqui pra próxima semana para deixar gigi feliz :)

--- aula_22/Projeto/service.py
+++ aula_22/Projeto/service.py
@@ -1,41 +1,31 @@
-from models.cliente import Cliente         # entidade
-from models.clientedao import ClienteDAO   # persistência
-from models.servico import Servico
-from models.servicodao import ServicoDAO
+from models.horario import Horario, HorarioDAO
 
 class Service:
+
     @staticmethod
-    def cliente_inserir(nome, email, fone):
-        obj = Cliente(0, nome, email, fone)
-        ClienteDAO().inserir(obj)
+    def horario_inserir(data, confirmado, id_cliente, id_servico):
+        c = Horario(0, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO().inserir(c)
+
     @staticmethod
-    def cliente_listar():
-        return ClienteDAO().listar()
+    def horario_listar():
+        return HorarioDAO().listar()
+
     @staticmethod
-    def cliente_listar_id(id):
-        return ClienteDAO().listar_id(id)
+    def horario_listar_id(id):
+        return HorarioDAO().listar_id(id)
+
     @staticmethod
-    def cliente_atualizar(id, nome, email, fone):
-        obj = Cliente(id, nome, email, fone)
-        ClienteDAO().atualizar(obj)
+    def horario_atualizar(id, data, confirmado, id_cliente, id_servico):
+        c = Horario(id, data)
+        c.set_confirmado(confirmado)
+        c.set_id_cliente(id_cliente)
+        c.set_id_servico(id_servico)
+        HorarioDAO().atualizar(c)
+
     @staticmethod
-    def cliente_excluir(id):
-        ClienteDAO().excluir(id)
-    
-    @staticmethod
-    def servico_inserir(descricao, valor):
-        obj = Servico(0, descricao, valor)
-        ServicoDAO().inserir(obj)
-    @staticmethod
-    def servico_listar():
-        return ServicoDAO().listar()
-    @staticmethod
-    def servico_listar_id(id):
-        return ServicoDAO().listar_id(id)
-    @staticmethod
-    def servico_atualizar(id, descricao, valor):
-        obj = Servico(id, descricao, valor)
-        ServicoDAO().atualizar(obj)
-    @staticmethod
-    def servico_excluir(id):
-        ServicoDAO().excluir(id)
+    def horario_excluir(id):
+        HorarioDAO().excluir(id)
